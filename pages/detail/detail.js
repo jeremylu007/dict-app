b9df374697ccef08656b2c291982dcfe10481881
luxinjian <luxinjian.cn@gmail.com>
2017-12-20 14:53:25 +0800

    Improve detail page

---
+++ pages/detail/detail.js
@@ -0,0 +1,27 @@
+
+Page({
+  data: {
+    wordInfo: {},
+    tabs: ["基本信息", "象形字典", "小学堂", "汉字叔叔"],
+    activeIndex: 1,
+    sliderOffset: -8,
+    sliderLeft: 0,
+  },
+  onLoad: function () {
+    var that = this
+    
+    wx.request({
+      url: 'http://47.93.97.73:3006/words?searchStr=汉',
+      success: function (res) {
+        var wordInfo = res.data.data[0]
+        that.setData({wordInfo: wordInfo })
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft - 8,
+      activeIndex: e.currentTarget.id
+    });
+  }
+});
