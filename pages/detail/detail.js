34f1d732d53ffccd99a51a7853e8c9b52b405d3c
luxinjian <luxinjian.cn@gmail.com>
2017-12-22 06:09:44 +0800

    Add more basic info

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,20 +4,21 @@ Page({
     wordInfo: {},
     tabs: ["基本信息", "象形字典", "小学堂", "汉字叔叔"],
     activeIndex: 0,
-    sliderOffset: -8,
+    sliderWidth: 100,
+    sliderOffset: 0,
     sliderLeft: 0,
   },
 
   onLoad: function(params) {
-    console.log(params)
+    // params.word = '兵' 
     wx.setNavigationBarTitle({ title: '“' + params.word + '”字的解释' })
     var that = this
-    
     wx.request({
       url: 'http://47.93.97.73:3006/words?searchStr=' + params.word,
       success: function (res) {
         var wordInfo = res.data.data[0]
         that.setData({wordInfo: wordInfo })
+        console.log(wordInfo)
       }
     });
   },
@@ -30,7 +31,7 @@ Page({
 
   tabClick: function (e) {
     this.setData({
-      sliderOffset: e.currentTarget.offsetLeft - 10,
+      sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
   }
