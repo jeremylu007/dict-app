694739298e7213abd805058a221855c4f815e451
luxinjian <luxinjian.cn@gmail.com>
2017-12-22 18:37:43 +0800

    Ready online!

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,21 +4,33 @@ Page({
     wordInfo: {},
     tabs: ["基本信息", "象形字典", "小学堂", "汉字叔叔"],
     activeIndex: 0,
-    sliderWidth: 98,
+    sliderWidth: 80,
     sliderOffset: 0,
     sliderLeft: 0,
   },
 
   onLoad: function(params) {
+    wx.showLoading({ title: '加载中' })
     // params.word = '兵' 
     wx.setNavigationBarTitle({ title: '“' + params.word + '”字的解释' })
     var that = this
     wx.request({
-      url: 'http://47.93.97.73:3006/words?searchStr=' + params.word,
+      url: 'https://vividict.cn/words?searchStr=' + params.word,
       success: function (res) {
+        wx.hideLoading()
         var wordInfo = res.data.data[0]
-        that.setData({wordInfo: wordInfo })
-        console.log(wordInfo)
+        if(!wordInfo) {
+          wx.showToast({ title: '该字尚未收录:(' })
+        } else {
+          that.setData({wordInfo: wordInfo })
+        }
+      }
+    });
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderWidth: res.windowWidth / that.data.tabs.length
+        });
       }
     });
   },
